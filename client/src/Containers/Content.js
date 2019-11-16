3d227c7e64debab6e505cb4b4421a03a5d8bec3b
christopherkalule <ckalule7@gmail.com>
2019-11-16 20:53:43 +0300

    Plant listing

--- client/src/Containers/Content.js
+++ client/src/Containers/Content.js
@@ -1,33 +1,61 @@
-import React from 'react';
-import {Listings} from './Listings';
+import React from "react"
+import { Listings } from "./Listings"
 
 export const Content = ({ search }) => {
-	return <div className="container-fluid content-div mt-5">
-		<nav>
-			<div className="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-				<a className="nav-item nav-link active" id="nav-home-tab" data-toggle="tab"
-				   href="#nav-home" role="tab" aria-controls="nav-home"
-				   aria-selected="true">Listings</a>
-				<a className="nav-item nav-link" id="nav-contact-tab" data-toggle="tab"
-				   href="#nav-profile" role="tab" aria-controls="nav-contact"
-				   aria-selected="false">At Glance</a>
-			</div>
-		</nav>
-		<div className="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
-			<div className="tab-pane fade show active" id="nav-home" role="tabpanel"
-				 aria-labelledby="nav-home-tab">
-				<Listings search={search}/>
-			</div>
-			<div className="tab-pane fade" id="nav-profile" role="tabpanel"
-				 aria-labelledby="nav-profile-tab">
-				Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat
-				qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam
-				sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure
-				incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do
-				nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit.
-				Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore.
-				Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
-			</div>
-		</div>
-	</div>
-};
+  return (
+    <div className="container-fluid content-div mt-5">
+      <nav>
+        <div className="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+          <a
+            className="nav-item nav-link active"
+            id="nav-home-tab"
+            data-toggle="tab"
+            href="#nav-home"
+            role="tab"
+            aria-controls="nav-home"
+            aria-selected="true"
+          >
+            Listings
+          </a>
+          <a
+            className="nav-item nav-link"
+            id="nav-contact-tab"
+            data-toggle="tab"
+            href="#nav-profile"
+            role="tab"
+            aria-controls="nav-contact"
+            aria-selected="false"
+          >
+            At Glance
+          </a>
+        </div>
+      </nav>
+      <div className="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+        <div
+          className="tab-pane fade show active"
+          id="nav-home"
+          role="tabpanel"
+          aria-labelledby="nav-home-tab"
+        >
+          <Listings search={search} />
+        </div>
+        <div
+          className="tab-pane fade"
+          id="nav-profile"
+          role="tabpanel"
+          aria-labelledby="nav-profile-tab"
+        >
+          Et et consectetur ipsum labore exB cepteur est proident excepteur ad
+          velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt
+          anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit
+          non irure adipisicing aliqua ullamco irure incididunt irure non esse
+          consectetur nostrud minim non minim occaecat. Amet duis do nisi duis
+          veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui
+          sit. Exercitation mollit sit culpa nisi culpa non adipisicing
+          reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad
+          duis occaecat ex.
+        </div>
+      </div>
+    </div>
+  )
+}
--- client/src/components/Listing.js
+++ client/src/components/Listing.js
@@ -2,9 +2,49 @@ import React, {Fragment} from 'react';
 
 export const Listing = ({ result }) => (
 	<Fragment>
-		<li className="list-group-item d-flex justify-content-between align-items-center">
-			Cras justo odio
-			<span className="badge badge-primary badge-pill">14</span>
-		</li>
+		<div class="card">
+			<div class="card-header" id={result.id}>
+				<h2 class="mb-0">
+					<button class="btn btn-link" type="button" data-toggle="collapse" data-target={`#${result.taxon_id}`} aria-expanded="true" aria-controls="collapseOne" style={{fontSize:"1.5rem"}}>
+						{result.scientific_name}
+					</button>
+				</h2>
+			</div>
+
+			<div id={result.taxon_id} class="collapse" aria-labelledby={result.id} data-parent="#accordionExample">
+				<div class="card-body">
+				<ul class="list-group list-group-flush">
+					<div className="detailList">
+						<li class="list-group-item font-weight-bold">Kingdom:</li>
+						<span>{result.kingdom}</span>
+					</div>
+					<div className="detailList">
+						<li class="list-group-item font-weight-bold">Scientific Name:</li>
+						<span>{result.scientific_name}</span>
+					</div>
+					<div className="detailList">
+						<li class="list-group-item font-weight-bold">Appendix:</li>
+						<span>{result.appendix}</span>
+					</div>
+					<div className="detailList">
+						<li class="list-group-item font-weight-bold">Author:</li>
+						<span>{result.author}</span>
+					</div>
+					<div className="detailList" style={{alignItems: "baseline"}}>
+						<li class="list-group-item font-weight-bold">Country Distribution</li>
+						<span>
+							<div className="countrydetails">
+								{result.country_distribution.map(country => (
+									<ul>
+										<li>{country}</li>
+									</ul>
+								))}
+							</div>
+						</span>
+					</div>
+				</ul>
+				</div>
+			</div>
+		</div>
 	</Fragment>
 );
