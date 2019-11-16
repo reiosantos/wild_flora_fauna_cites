2e59cab00b1f700249c8e58ce9c0c2fe36be1e9c
Reiosantos <ronireiosantos@gmail.com>
2019-11-16 16:30:29 +0300

    Project temp

---
+++ client/src/components/Listing.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+
+export const Listing = ({ result }) => (
+	<Fragment>
+		<li className="list-group-item d-flex justify-content-between align-items-center">
+			Cras justo odio
+			<span className="badge badge-primary badge-pill">14</span>
+		</li>
+	</Fragment>
+);
